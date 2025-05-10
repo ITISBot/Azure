54a40a72dbda58e5defa3a01d786dbab2a4f1f47
ITISBot <62021622+ITISBot@users.noreply.github.com>
2025-05-10 19:53:30 -0400

    Create vnet.tf

---
+++ vnet/vnet.tf
@@ -0,0 +1,40 @@
+provider "azurerm" {
+  features {}
+}
+
+# Define the Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "West Europe" # Change to your desired region
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Define Subnets
+resource "azurerm_subnet" "prod" {
+  name                 = "prod-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "preprod" {
+  name                 = "preprod-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "dev" {
+  name                 = "dev-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_subnet" "lab" {
+  name                 = "lab-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.4.0/24"]
+}
