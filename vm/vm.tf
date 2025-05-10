929ce3180e8f779c399d0912a38f4cca64c739b2
ITISBot <62021622+ITISBot@users.noreply.github.com>
2025-05-10 19:54:47 -0400

    Create vm.tf

---
+++ vm/vm.tf
@@ -0,0 +1,45 @@
+# Ubuntu Linux Virtual Machine
+resource "azurerm_network_interface" "nic" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.prod.id # Attach to the Prod subnet
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "example-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_B1s"
+
+  # Use the latest Ubuntu image
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "example-os-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "example-vm"
+    admin_username = "adminuser"
+    admin_password = "P@ssw0rd1234!" # Replace with a secure password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
